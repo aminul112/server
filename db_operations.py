dcff4c8828e17b5efc9320ed398e100de3410e0f
Aminul Chowdhury <aminul.c@covergenius.com>
2023-01-04 21:35:43 +1000

    Wrapping database operations in to a class with log injection.

--- db_operations.py
+++ db_operations.py
@@ -1,70 +1,71 @@
-import logging
 
 import asyncpg
 
 
-async def query_saved_clients_from_db():
-    conn = None
-    log = logging.getLogger("server_log")
-    try:
-        log.info("query_saved_clients_from_db()")
-        conn = await asyncpg.connect(user='devuser', password='devpwd',
-                                     database='devdb', host='0.0.0.0', port=5432)
+class AsyncPgPostgresManager:
+    def __init__(self, log):
+        self.log = log
 
-        # Execute a statement to create a new table if necessary
-        await conn.execute('''
-           CREATE TABLE IF NOT EXISTS client_record (
-             client_identifier INTEGER NOT NULL,
-             status_count INTEGER,
-             is_connected BOOLEAN NOT NULL,
-             PRIMARY KEY (client_identifier , is_connected),             
-             client_host  CHAR(200) NOT NULL,             
-             client_port INTEGER NOT NULL,
-             connection_time  TIMESTAMP NOT NULL DEFAULT NOW()
-             )
-       ''')
+    async def query_saved_clients_from_db(self):
+        conn = None        
+        try:
+            self.log.info("query_saved_clients_from_db()")
+            conn = await asyncpg.connect(user='devuser', password='devpwd',
+                                         database='devdb', host='0.0.0.0', port=5432)
 
-        # get all active clients
-        rows = await conn.fetch(
-            'SELECT * FROM client_record WHERE is_connected = $1', True)
-
-        active_clients = [dict(row) for row in rows]
-
-        # Close the connection
-        await conn.close()
-        return active_clients
-    except Exception as e:
-        if conn:
-            await conn.close()
-        log.error(f"Database exception happened {e}")
-        return []
-
-
-async def update_client_list_to_db(updated_clients_mapping):
-    conn = None
-    log = logging.getLogger("server_log")
-    try:
-        log.info("update_client_list_to_db")
-        conn = await asyncpg.connect(user='devuser', password='devpwd',
-                                     database='devdb', host='0.0.0.0', port=5432)
-
-        async with conn.transaction():
-
+            # Execute a statement to create a new table if necessary
             await conn.execute('''
-                           TRUNCATE client_record
-                       ''')
+               CREATE TABLE IF NOT EXISTS client_record (
+                 client_identifier INTEGER NOT NULL,
+                 status_count INTEGER,
+                 is_connected BOOLEAN NOT NULL,
+                 PRIMARY KEY (client_identifier , is_connected),             
+                 client_host  CHAR(200) NOT NULL,             
+                 client_port INTEGER NOT NULL,
+                 connection_time  TIMESTAMP NOT NULL DEFAULT NOW()
+                 )
+           ''')
 
-            for client_id in updated_clients_mapping:
-                host = updated_clients_mapping[client_id].get("client_host")
-                port = updated_clients_mapping[client_id].get("client_port")
-                status_count = updated_clients_mapping[client_id].get("status_count")
-                await conn.execute('''
-                   INSERT INTO client_record(client_identifier, is_connected, client_host, client_port, status_count) VALUES($1, $2, $3, $4, $5)
-               ''', client_id, True, host, port, status_count)
+            # get all active clients
+            rows = await conn.fetch(
+                'SELECT * FROM client_record WHERE is_connected = $1', True)
 
-        await conn.close()
-    except Exception as e:
-        if conn:
+            active_clients = [dict(row) for row in rows]
+
+            # Close the connection
             await conn.close()
-        log.error(f"Database exception happened {e}")
-        return []
+            return active_clients
+        except Exception as e:
+            if conn:
+                await conn.close()
+            self.log.error(f"Database exception happened {e}")
+            return []
+
+    async def update_client_list_to_db(self, updated_clients_mapping):
+        conn = None
+        
+        try:
+            self.log.info("update_client_list_to_db")
+            conn = await asyncpg.connect(user='devuser', password='devpwd',
+                                         database='devdb', host='0.0.0.0', port=5432)
+
+            async with conn.transaction():
+
+                await conn.execute('''
+                               TRUNCATE client_record
+                           ''')
+
+                for client_id in updated_clients_mapping:
+                    host = updated_clients_mapping[client_id].get("client_host")
+                    port = updated_clients_mapping[client_id].get("client_port")
+                    status_count = updated_clients_mapping[client_id].get("status_count")
+                    await conn.execute('''
+                       INSERT INTO client_record(client_identifier, is_connected, client_host, client_port, status_count) VALUES($1, $2, $3, $4, $5)
+                   ''', client_id, True, host, port, status_count)
+
+            await conn.close()
+        except Exception as e:
+            if conn:
+                await conn.close()
+            self.log.error(f"Database exception happened {e}")
+            return []
